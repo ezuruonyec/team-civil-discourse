f6649f730838f89bc074a3ac9ad937743af5d883
Carson Uecker-Herman <ueckerhc@mail.gvsu.edu>
2020-09-20 14:57:50 -0400

    added header

--- civil-map/src/App.js
+++ civil-map/src/App.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
-import ReactTooltip from "react-tooltip";
+import React, {useState} from 'react'
+import ReactTooltip from "react-tooltip"
 import Map from "./components/Map"
-
+import "./App.css"
 
 function App() {
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState("")
   return (
     <div>
+      <h1>Welcome to our Map</h1>
       <Map setTooltipContent={setContent}/>
       <ReactTooltip>{content}</ReactTooltip>
     </div>
