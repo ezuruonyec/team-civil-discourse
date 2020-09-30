ac0f6b306d6c94010e4861da306c049344087c66
Carson Uecker-Herman <ueckerhc@mail.gvsu.edu>
2020-09-29 22:47:17 -0400

    separated dev keys and prod keys.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
-module.exports = {
-    mongoURI: "mongodb+srv://carson:teamcivilmap2020@cluster0.et1lq.mongodb.net/data?retryWrites=true&w=majority"
+if(process.env.NODE_ENV === "production") {
+    //return prod keys
+    module.exports = require("./prod")
+}
+else {
+    //return dev keys
+    module.exports = require("./dev")
 }
 
 
