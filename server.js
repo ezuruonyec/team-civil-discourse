e0923ee83433d6ed7527e9500682473f1b2412c7
Carson Uecker-Herman <ueckerhc@mail.gvsu.edu>
2020-10-03 23:10:47 -0400

    Heroku deploy test

--- server.js
+++ server.js
@@ -23,7 +23,13 @@ mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true } )
 // use routes
 app.use("/api/countries", countries)
 
-
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000
 
