8c91971ea7b958b51f21fb6f372e1459d6529cd5
Carson Uecker-Herman <ueckerhc@mail.gvsu.edu>
2020-10-03 23:34:21 -0400

    More changes

--- server.js
+++ server.js
@@ -23,13 +23,15 @@ mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true } )
 // use routes
 app.use("/api/countries", countries)
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client', 'build')));
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
   
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-    
   }
 
 const port = process.env.PORT || 5000
