4c93b7f385314316ccbc7f32dee8ff3fb5cacfe4
Aaron Bager <kingjojo3@gmail.com>
2021-02-12 20:22:58 -0500

    Fixed up
    Added if statement to change variables based on specific country calls.
    Experimented with keywords. Tried to see if I could retrieve New York
    times articles within NewsAPI, no luck so far

--- backend-resources/news.py
+++ backend-resources/news.py
@@ -6,18 +6,21 @@ import config
 def news():
     print('Starting Program') 
     # keywords = ['fake news', 'misinformation', 'civil discourse', 'freedom of speech'] # keywords to search for with news articles
-    keywords = 'fake news OR misinformation OR civil discourse OR freedom of speech'
+    keywords = 'fake news OR misinformation OR civil discourse OR freedom of speech OR journalism'
     domains = 'washingtonpost.com,nytimes.com'
     for country in sys.argv[1:]: # countries should be passed via command line? 
         # TODO figure out how countries should be inputted? Should they all be seperate scripts? if statements?
         print(country)
-        request_url = 'https://newsapi.org/v2/everything?q=' + keywords + '&domains=' + domains +'&pageSize=5&apiKey=' + config.newsAPI_key
-        news_request = requests.get(request_url)
-        print(news_request)
+        if country == 'United-States':
+            domains ='washingtonpost.com,nytimes.com'
+            request_url = ('https://newsapi.org/v2/everything?q=' + keywords  
+            + '&domains=' + domains +'&pageSize=15&apiKey=' + config.newsAPI_key)
+            news_request = requests.get(request_url)
+            print(news_request)
         # TODO: send to cache instead of writing to file on local system
-        f = open('return.json', 'w')
-        f.write(news_request.text)
-        f.close()
+            f = open('return.json', 'w')
+            f.write(news_request.text)
+            f.close()
 
 def main():
     news()
