d524dfcdf798201a626829df2ebcf8133118779f
omartiba <55855727+omartiba@users.noreply.github.com>
2021-02-18 14:28:22 -0500

    Add files via upload

---
+++ ArticleCard.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: theme.spacing(3)
+    },
+
+    title: {
+        fontSize: theme.spacing(5),
+        textAlign: "center",
+        fontFamily: "Roboto",
+    },
+
+    detail: {
+        fontSize: theme.spacing(4),
+        textAlign: "left",
+        fontFamily: "Times New Roman",
+    }, 
+
+   subDetail: {
+        fontSize: theme.spacing(2),
+        textAlign: "left",
+        fontFamily: "Times New Roman",
+   },
+
+   author: {
+        fontSize: theme.spacing(2),
+        textAlign: "left",
+        fontFamily: "Times New Roman",
+   },
+
+
+
+}))
+
+function ArticleCard({title, detail, subDetail,author}) {
+
+    const classes = useStyles();
+
+    return(
+        <Paper className={classes.root}>
+            <Paper className={classes.title} elevation={0}>{title}</Paper>
+            <Paper className={classes.detail} elevation={0}>{detail}</Paper>
+            <Paper className={classes.subDetail} elevation={0}>{subDetail}</Paper>
+            <Paper className={classes.author} elevation={0}>{author}</Paper>
+        </Paper>
+    )
+}
+
+export default ArticleCard
---
+++ countries.js
@@ -0,0 +1,133 @@
+const express = require("express")
+const { count } = require("../../models/Country")
+const router = express.Router()
+var article_arr = null
+const fs = require('fs')
+fs.readFile('./dummy_file.json', 'utf8', (err, jsonString) => {
+    if(err) {
+        console.log("File read Failed:", err)
+        return
+    }
+    try {
+        const parsed_data = JSON.parse(jsonString)
+        //console.log('Article Data:', parsed_data.articles)
+        article_arr = parsed_data.articles 
+        console.log('Articel Array:', article_arr)
+    } catch(err) {
+        console.log("Error parsing JSON string:", err)
+    }
+    
+})
+
+// item model
+const Country = require("../../models/Country")
+
+// @route GET api/countries
+// @desc Get All Countries
+// @access Public
+router.get("/", (req, res) => {
+    Country.find()
+        .sort({name: 1})
+        .then(country => res.json(country))
+})
+
+// @route   GET api/countries
+// @desc    Create countries
+// @access  Public
+router.post("/", (req, res) => {
+    const newCountry = new Country({
+        name: req.body.name,
+        two_digit: req.body.two_digit,
+        three_digit: req.body.three_digit,
+        cd_rating: req.body.cd_rating,
+        cd_ranking: req.body.cd_ranking,
+        rwb_score: req.body.rwb_score,
+        rwb_ranking: req.body.rwb_ranking,
+        internet_access: req.body.internet_access,
+        internet_access_ranking: req.body.internet_access_ranking,
+        internet_access_year: req.body.internet_access_year,
+        censorship_level: req.body.censorship_level,
+        censorship_ranking: req.body.censorship_ranking,
+        millenium_dec_ranking: req.body.millenium_dec_ranking,
+        millenium_dec_ratified: req.body.millenium_dec_ratified,
+        millenium_dec_year: req.body.millenium_dec_year,
+        population: req.body.population,
+        //article_array: req.body.article_arr,
+        // sources: req.body.sources,
+        // freedom_speech: req.body.freedom_speech,
+        // freedom_media: req.body.freedom_media,
+        // fake_news: req.body.fake_news,
+        poverty_level: req.body.poverty_level,
+    })
+
+    newCountry.save()
+        .then(item => res.json(item))
+})
+
+// @route   GET api/countries
+// @desc    Get a single country by the country code
+// @access  Public
+
+// router.get("/:code", (req, res) => {
+//     Country.findOne({code: req.params.code})
+//         .then(country => res.json(country))
+// })
+
+// @route   PUT api/countries/update
+// @desc    Update a country
+// @access  Public
+router.put("/update", (req, res) => {
+    console.log(req.body)
+    Country.findByIdAndUpdate(req.body.id, {
+        name: req.body.name,
+        two_digit: req.body.two_digit,
+        three_digit: req.body.three_digit,
+        cd_rating: req.body.cd_rating,
+        cd_ranking: req.body.cd_ranking,
+        rwb_score: req.body.rwb_score,
+        rwb_ranking: req.body.rwb_ranking,
+        internet_access: req.body.internet_access,
+        internet_access_ranking: req.body.internet_access_ranking,
+        internet_access_year: req.body.internet_access_year,
+        censorship_level: req.body.censorship_level,
+        censorship_ranking: req.body.censorship_ranking,
+        millenium_dec_ranking: req.body.millenium_dec_ranking,
+        millenium_dec_ratified: req.body.millenium_dec_ratified,
+        millenium_dec_year: req.body.millenium_dec_year,
+        population: req.body.population,
+        poverty_level: req.body.poverty_level,
+        //article_array: req.body.article_arr,
+        updated: Date.now()
+        // sources: req.body.sources,
+        // freedom_speech: req.body.freedom_speech,
+        // freedom_media: req.body.freedom_media,
+        // fake_news: req.body.fake_news,
+    },
+    {upsert: true}
+    )
+    .then(res.json({sucesss:true}))
+    .catch(err => res.status(404).json({sucess: false}))
+})
+
+
+// @route   DELELTE api/countries
+// @desc    Delete country
+// @access  Public
+router.delete("/:id", (req, res) => {
+   Country.findByIdAndDelete({_id: req.params.id})
+   .then(res.json({sucesss:true}))
+   .catch(err => res.status(404).json({sucess: false}))
+})
+
+// @route GET api/countries/:name
+// @desc Get All Countries
+// @access Public
+router.get("/name/:name", (req, res) => {
+    Country.findOne({name: req.params.name})
+        .then(country => res.json(country))
+})
+
+    
+
+
+module.exports = router
