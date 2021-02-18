4d07ae75cc85ca1d2d677ab5dbad6a09d7da4004
omartiba <55855727+omartiba@users.noreply.github.com>
2021-02-18 10:33:58 -0500

    Added Additional Article Card Format

---
+++ client/src/components/ArticleCard.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: theme.spacing(3)
+    },
+
+    title: {
+        fontSize: theme.spacing(5),
+        textAlign: "left",
+        fontFamily: "Roboto",
+    },
+
+    detail: {
+        fontSize: theme.spacing(4),
+        textAlign: "center",
+        fontFamily: "Roboto",
+    }, 
+
+   subdetail: {
+        fontSize: theme.spacing(2),
+        textAlign: "right",
+        fontFamily: "Times New Roman",
+   }
+
+//    source: {
+       
+//    },
+
+//    author: {
+
+//    },
+
+//    content: {
+
+//    },
+
+//    image: {
+
+//    }
+
+}))
+
+function ArticleCard({title, detail, subDetail}) {
+
+    const classes = useStyles();
+
+    return(
+        <Paper className={classes.root}>
+            <Paper className={classes.title} elevation={0}>{title}</Paper>
+            <Paper className={classes.detail} elevation={0}>{detail}</Paper>
+            <Paper className={classes.subDetail} elevation={0}>{subDetail}</Paper>
+        </Paper>
+    )
+}
+
+export default ArticleCard
