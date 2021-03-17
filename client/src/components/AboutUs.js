6e94ca33edd0e6498b4fd82af30a18a766adab70
Stephen <17kisern@gmail.com>
2021-03-17 02:47:59 -0400

    Updated/Fixed NPM errors

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -46,6 +46,8 @@ export function AboutUs() {
                 </p>
                 <p>
                     The CIS students during the Fall 2020 semester: Josh Lowell, Carson Uecker-Herman, and Olivia Vitali.
+                </p>
+                <p>
                     The CIS students during the Winter 2020 semester: Aaron Bager, Stephen Kiser, Nicholas Sheehan, Omar Tiba.
                 </p>
                 </Container>
