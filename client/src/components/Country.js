c092ae18b4425d7500439066d6639755fd2ccc21
Aaron Bager <kingjojo3@gmail.com>
2021-03-16 22:55:20 -0400

    Took out hard coded links to the article images
    They are now soft linked via variables

--- client/src/components/Country.js
+++ client/src/components/Country.js
@@ -55,7 +55,7 @@ function Country({id, name, two_digit, three_digit, population, millenium_dec_ra
                   article_2_title, article_2_description, article_2_author, article_2_date, article_2_source, article_2_url,
                   article_3_title, article_3_description, article_3_author, article_3_date, article_3_source, article_3_url,
                   article_4_title, article_4_description, article_4_author, article_4_date, article_4_source, article_4_url,
-                  article_5_title, article_5_description, article_5_author, article_5_date, article_5_source, article_5_url}) {
+                  article_5_title, article_5_description, article_5_author, article_5_date, article_5_source, article_5_url, article_1_image_url, article_2_image_url, article_3_image_url}) {
     const classes = useStyles();
     
     return (
@@ -114,17 +114,17 @@ function Country({id, name, two_digit, three_digit, population, millenium_dec_ra
             <Grid container spacing={1}>
                 <Grid item xs={12} sm={3}>
                     <ArticleCard title = {article_2_title} author = {article_2_author} date = {article_2_date} articleUrl = {article_2_url} description = {article_2_description} 
-                    imageUrl = "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/WHSI4BGFHEI6VKBFQ4RAATSBKA.jpg&w=1440" />
+                    imageUrl = {article_1_image_url}/>
                 </Grid>
 
                 <Grid item xs={12} sm={3}>
                     <ArticleCard title = {article_1_title} author = {article_1_author} date = {article_1_date} articleUrl = {article_1_url} description = {article_1_description}
-                    imageUrl = "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/MJMDWZSUTYI6XKAX4XT7RJAG2Y.jpg&w=1440"/>
+                    imageUrl = {article_2_image_url}/>
                 </Grid>
 
                 <Grid item xs={12} sm={3}>
                     <ArticleCard title = {article_3_title} author = {article_3_author} date = {article_3_date} articleUrl = {article_3_url} description = {article_3_description}
-                    imageUrl = "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/U552NKDA5II6XILXO5S7FGUVEQ.jpg&w=1440"/>
+                    imageUrl = {article_3_image_url}/>
                 </Grid>
             </Grid>
             
