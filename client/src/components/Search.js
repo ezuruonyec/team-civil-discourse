607aede493fcb294a4f142c8e6659830aae73722
Aaron <kingjojo3@gmail.com>
2021-03-20 22:22:03 -0400

    Got all countries working again
    There is now a seperate check to see if articles return. If there is no
    return, articles will not display

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -30,15 +30,12 @@ function Search() {
 
             // Construct our results data to be both the attributes and news data
             //const mergedResponse = attributes.data + articles.data;
-	    if (articles != null || articles != undefined) {
             	const mergedResponse = Object.assign(attributes.data, articles.data)
             // Update our state
             	setResults(mergedResponse);
             	setLoading(false);
-	    } else {
-		setResults(attributes)
-		setLoading(false)
-	    }
+		//setResults(attributes)
+		//setLoading(false)
             return
         } catch (error) {
             console.log(error);
@@ -46,20 +43,6 @@ function Search() {
         }
     }
 
-    /*async function getArticleByName(countryName) {
-		try {
-			let articlesRequestURL = 'https://h5kxmgz3lc.execute-api.us-east-1.amazonaws.com/development/CivilDiscourseMap-GetNewsByName?CountryName=' + countryName;
-	 		var articleHandler = axios.get(articlesRequestURL); 
-			const articles = await articleHandler; 
-			const mergedResponse = articles.data
-			setLoading(false); 
-			return 
-		}
-	        catch (error) {
-			console.log(error); 
-			console.log(error.response); 
-		}
-    }*/
 
     useEffect(() => { getCountryByName(term); }, [term])
 
@@ -78,6 +61,27 @@ function Search() {
                         <h1>Not found: {term} </h1>
                         :
                         // valid search term. Country was found, display Country Component
+		     results['articles'] === null || results['articles'] === undefined ?
+
+			<Country
+                            // key={item._id}
+                            name={results['CountryName']}
+                            two_digit={results['CountryCode']}
+                            population={results['Population']}
+                            millenium_dec_ratified={results['MilleniumDeclarationRatified']}
+                            millenium_dec_year={results['MilleniumDeclarationYear']}
+                            rwb_rank={results['RwbRank']}
+                            rwb_score={results['RwbScore']}
+                            internet_access={results['InternetAccessPercent']}
+                            internet_access_ranking={results['InternetAccessRank']}
+                            internet_access_year={results['InternetAccessYear']}
+                            censorship_level={results['CensorshipLevel']}
+                            censorship_ranking={results['CensorshipRank']}
+                            cd_rating={results['DiscourseRating']}
+                            cd_ranking={results['DiscourseRanking']}
+
+		        /> 
+		       :
 
                         <Country
                             // key={item._id}
@@ -95,27 +99,26 @@ function Search() {
                             censorship_ranking={results['CensorshipRank']}
                             cd_rating={results['DiscourseRating']}
                             cd_ranking={results['DiscourseRanking']}
-                            article_1_title={results['articles'][0]['title']}
-                            article_2_title={results['articles'][1]['title']}
-                            article_3_title={results['articles'][2]['title']}
-                            article_4_title={results['articles'][3]['title']}
-                            article_1_description={results['articles'][0]['description']}
-		    	    article_2_description={results['articles'][1]['description']}
-		    	    article_3_description={results['articles'][2]['description']}
-		    	    article_4_description={results['articles'][3]['description']}
-		            article_1_url={results['articles'][0]['url']}
-		    	    article_2_url={results['articles'][1]['url']}
-		            article_3_url={results['articles'][2]['url']}
-		            article_4_url={results['articles'][3]['url']} 
-			    article_1_date={results['articles'][0]['publishedAt']}
-			    article_2_date={results['articles'][1]['publishedAt']}
-			    article_3_date={results['articles'][2]['publishedAt']}
-			    article_4_date={results['articles'][3]['publishedAt']}
-		            article_1_image_url={results['articles'][0]['urlToImage']}
-		            article_2_image_url={results['articles'][1]['urlToImage']}
-		            article_3_image_url={results['articles'][2]['urlToImage']}
-		            article_4_image_url={results['articles'][3]['urlToImage']}
-
+		    		article_1_title={results['articles'][0]['title']}
+                            	article_2_title={results['articles'][1]['title']}
+                            	article_3_title={results['articles'][2]['title']}
+                            	article_4_title={results['articles'][3]['title']}
+                            	article_1_description={results['articles'][0]['description']}
+		    	    	article_2_description={results['articles'][1]['description']}
+		    	    	article_3_description={results['articles'][2]['description']}
+		    	    	article_4_description={results['articles'][3]['description']}
+		            	article_1_url={results['articles'][0]['url']}
+		    	    	article_2_url={results['articles'][1]['url']}
+		           	article_3_url={results['articles'][2]['url']}
+		            	article_4_url={results['articles'][3]['url']} 
+			    	article_1_date={results['articles'][0]['publishedAt']}
+			    	article_2_date={results['articles'][1]['publishedAt']}
+			    	article_3_date={results['articles'][2]['publishedAt']}
+			    	article_4_date={results['articles'][3]['publishedAt']}
+		            	article_1_image_url={results['articles'][0]['urlToImage']}
+		            	article_2_image_url={results['articles'][1]['urlToImage']}
+		            	article_3_image_url={results['articles'][2]['urlToImage']}
+		            	article_4_image_url={results['articles'][3]['urlToImage']}
 
 		    // TODO  
                         /*            article_array={item.article_array}
